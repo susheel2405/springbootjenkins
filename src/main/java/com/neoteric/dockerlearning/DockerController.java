541124057f36952c06343e9601f74973c4510672
Tirumala <tirumalatamma17@gmail.com>
2024-10-08 14:55:31 +0530

    spring boot jenkins

--- src/main/java/com/neoteric/dockerlearning/DockerController.java
+++ src/main/java/com/neoteric/dockerlearning/DockerController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
     @GetMapping("/dockerDemo")
     public String getHelloWorld() {
-        return "Docker Demo";
+        return "Docker Demo with jenkins";
     }
 }
