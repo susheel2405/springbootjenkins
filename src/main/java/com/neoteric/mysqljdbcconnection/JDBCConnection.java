22eca1e167500db82aaac0cf6747bc5bcabd46b8
susheel2405 <susheelchinnup23@gmail.com>
2024-12-09 09:12:13 +0530

    hello world

--- src/main/java/com/neoteric/mysqljdbcconnection/JDBCConnection.java
+++ src/main/java/com/neoteric/mysqljdbcconnection/JDBCConnection.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class JDBCConnection {
+
     public  static List<Project> getAllocationEmployees(){
 
         String url = "jdbc:mysql://localhost:3306/sys";
